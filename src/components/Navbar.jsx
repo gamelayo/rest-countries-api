5eb139f32bd5fec145e294b9af1ace396dd818df
Gamel Ayodele <ayodelegamel@gmail.com>
2023-04-27 22:50:55 +0100

    latest commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import ThemeToggle from "./ThemeToggle";
 const Navbar = () => {
   return (
     <header>
-      <nav className="flex justify-between px-8 items-center h-20 py-2 bg-primary">
+      <nav className="flex justify-between px-3 md:px-8 items-center h-20 py-2 bg-primary">
         <div>
           <h1 className="font-bold text-xl">Where in the world?</h1>
         </div>
